f5aec9338ec32ae6d356b827e887814a4a57930a
Leandro <leandrowebmaster@gmail.com>
2025-06-13 15:06:47 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -283,10 +283,40 @@ alignItems: 'center',
             functions:[async (...args) =>
  functions.funcGroup({ args, pass:{
  arrFunctions: [() => {
+  const path1 = "scA0.gameList";
+  const path2 = "sc.A1.wishlist";
+  const allVal = tools.getCtData(path1);
 
-tools.functions.getCtData("scA0.gameList") 
+  const { rendTrib, desMed, irRet, edu, prev, dep } = allVal;
+console.log({irRet});
 
+  const toNum = (str) => parseFloat((str || "0").replace(",", "."));
 
+  const totalDep = toNum(dep) * 2400;
+  const totalDed = toNum(desMed) + toNum(edu) + toNum(prev) + totalDep;
+  const calcBase = Math.max(0, toNum(rendTrib) - totalDed);
+
+  const tabelaIR = tools.getCtData('all.tabelaIR');
+  const faixaEncontrada = tabelaIR.find(faixa => calcBase <= faixa.limite);
+
+const irDevido = Math.max(0, (calcBase * (faixaEncontrada.aliquota / 100)) - faixaEncontrada.deducao);
+
+const resultado = toNum(irRet) - irDevido;
+
+let finalResult;
+if (resultado >= 0) {
+const res1 = "Restituição: R$" + resultado.toFixed(2);
+  console.log( res1 );
+finalResult = res1;
+} else {
+  const res2 = "Imposto a pagar: R$" +  Math.abs(resultado).toFixed(2);
+  console.log( res2 );
+finalResult = res2;
+}
+console.log({finalResult});
+
+ const pass1 = { keyPath: [path2], value: [finalResult] };
+ tools.functions.setVar({ args: '', pass: pass1 });
 }]
  , trigger: 'on press'
 }})],            childrenItems:[(...args:any) => <Elements.Text pass={{
