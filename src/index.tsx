841c0205fca06907c374fc081a43ee3a1c891b49
Carlos A. <carlosvsk@gmail.com>
2025-06-23 16:29:37 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1334,7 +1334,7 @@ justifyContent: 'center',
                     onPress={() => {
                     
   if (RN.Platform.OS === 'web') {
-window.open("https://www.cheapshark.com/redirect?dealID=" + item.dealID, '_blank');
+window.open("https://www.cheapshark.com/redirect?dealID=" + item.dealID, '_self');
   } else {
 RN.Linking.openURL("https://www.cheapshark.com/redirect?dealID=" + item.dealID);
   }
